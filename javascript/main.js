4d922a1c5092dc2854c766b19336f750c8519b42
zainabg1 <2205208@talnet.nl>
2026-06-06 00:33:41 +0200

    javascript cv laten werken

--- javascript/main.js
+++ javascript/main.js
@@ -1,1 +1,12 @@
-+
+/*Javascript code CV downloaden*/
+document.getElementById('download-cv').addEventListener('click', e => {
+    e.preventDefault();
+
+    const link = document.createElement('a');
+    link.href = '../assets/Zainab-Guman-CV.pdf'; 
+    link.download = 'Zainab-Guman-CV.pdf'; 
+    document.body.appendChild(link);
+    link.click();
+    document.body.removeChild(link);
+});
+/*Javascript code CV downloaden*/
